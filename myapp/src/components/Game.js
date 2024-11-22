7c8e254e645a57b8effb01682819e39a58e81c29
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-22 11:35:19 +0000

    Display results

--- myapp/src/components/Game.js
+++ myapp/src/components/Game.js
@@ -16,4 +16,28 @@ export default function Game(){
             setResult("Lose");
         }
     }
+
+    const restartGame=()=>{
+        setTargetAnimal(getRandomAnimal());
+        setResult(null);
+    };
+
+    return(
+        <>
+        <div className="game-page">
+            <div>
+                <h1>Animal Matching Game</h1>
+            </div>
+            <div>
+                <div>
+                    {result && (
+                        <div className="result">
+                            <h2>Result</h2>
+                            <h2>{result}</h2>
+                            <button onClick={restartGame}>Play Again</button>
+                        </div>
+                    )}
+                </div>
+        </>
+    )
 }
