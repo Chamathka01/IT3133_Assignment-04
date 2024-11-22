5a49ab5f566bced72ccdfeafd65b6c8701ba4714
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-22 12:03:44 +0000

    Fixing some errors

--- myapp/src/components/Game.js
+++ myapp/src/components/Game.js
@@ -2,20 +2,20 @@ import { useState } from "react";
 import animals from "../data/AnimalsDb";
 
 export default function Game(){
-    const [targetAnimal, setTargetAnimal]=useState(null);
+    const [targetAnimal, setTargetAnimal] = useState(getRandomAnimal());
     const [result, setResult] = useState(null);
 
     function getRandomAnimal(){
         const randomIndex=Math.floor(Math.random()*animals.length);
-        return animals[randomIndex];
+        return {...animals[randomIndex],id:randomIndex};
     }
-    const handleClick=(id)=>{
-        if(id==targetAnimal.id){
+    const handleAnimalClick=(index)=>{
+        if(index==targetAnimal.id){
             setResult("Win");
         }else{
             setResult("Lose");
         }
-    }
+    };
 
     const restartGame=()=>{
         setTargetAnimal(getRandomAnimal());
@@ -25,35 +25,33 @@ export default function Game(){
     return(
         <>
         <div className="game-page">
-            <div>
                 <h1>Animal Matching Game</h1>
-            </div>
-            <div>
-                <div>
-                    {result && (
+                        {result && (
                         <div className="result">
-                            <h2>Result</h2>
+                            <h2>Result:{Result}</h2>
                             <h2>{result}</h2>
                             <button onClick={restartGame}>Play Again</button>
                         </div>
                     )}
                     <div className="game-header">
                     <h2>Animal Name: {targetAnimal.name}</h2>
-                </div>
+                    </div>
                 <div className="animal-grid">
-                    {animals.map((animal)=>(
+                    {animals.map((animal,index)=>(
                         <div
-                            key={animal.id}
+                            key={index}
                             className="animal-card"
-                            onClick={()=>handleAnimalClick(animal.id)}>
+                            onClick={()=>handleAnimalClick(index)}>
                                 <img
-                                src={require(`./img/${animal.img}`)}
+                                src={require(`.assets/img/${animal.img}`)}
+                                alt={animal.name}
+                                onError={(e) => (e.target.style.display = "none")}
                                 />
                         </div>
                     ))}
                 </div>
             </div>
-                </div>
+    
         </>
-    )
+    );
 }
