2b843ea55c94bbb8e1a6133b661113ce5ef485eb
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-11-23 03:56:44 +0000

    Fix import

--- myapp/src/components/Game.js
+++ myapp/src/components/Game.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {animals} from "../data/AnimalsDb";
+import '../assets/css/game.css'
 
 export default function Game(){
     const [targetAnimal, setTargetAnimal] = useState(getRandomAnimal());
